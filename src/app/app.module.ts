08cfb0811f23d7a87970a7e9025e92e3e86422fe
Mahdi <mahdibouaziz@hotmail.fr>
2021-04-17 14:23:03 +0000

    typographie in angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { TypographyComponent } from './typography/typography.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TypographyComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
