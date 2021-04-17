cb692117088d7b5935767e59e9bb32d7ed8bab31
Mahdi <mahdibouaziz@hotmail.fr>
2021-04-17 16:01:50 +0000

    icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
 
 @NgModule({
-  declarations: [AppComponent, TypographyComponent],
+  declarations: [AppComponent, TypographyComponent, IconsComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 // this module is responsible for only importing and exporting material components
 
-const MaterialComponents = [MatSliderModule, MatButtonModule];
+const MaterialComponents = [MatSliderModule, MatButtonModule, MatIconModule];
 
 @NgModule({
   imports: [MaterialComponents],
